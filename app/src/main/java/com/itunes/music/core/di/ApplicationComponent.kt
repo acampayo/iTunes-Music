50d10247208ecd7cdee92add65b2209f9598a57f
Alex Campayo <alex.campayo@gmail.com>
2018-12-04 01:10:16 +0100

    Create an inject SongsActivity

--- app/src/main/java/com/itunes/music/core/di/ApplicationComponent.kt
+++ app/src/main/java/com/itunes/music/core/di/ApplicationComponent.kt
@@ -1,12 +1,15 @@
 package com.itunes.music.core.di
 
 import com.itunes.music.AndroidApplication
+import com.itunes.music.features.songs.SongsActivity
 import com.tvshows.core.di.ApplicationModule
+import com.tvshows.core.di.viewmodel.ViewModelModule
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [ApplicationModule::class])
+@Component(modules = [ApplicationModule::class, ViewModelModule::class])
 interface ApplicationComponent {
     fun inject(application: AndroidApplication)
+    fun inject(songsActivity: SongsActivity)
 }
--- app/src/main/java/com/itunes/music/features/songs/SongsActivity.kt
+++ app/src/main/java/com/itunes/music/features/songs/SongsActivity.kt
@@ -1,13 +1,49 @@
 package com.itunes.music.features.songs
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.itunes.music.R
+import com.itunes.music.core.exception.Failure
 import com.itunes.music.core.platform.BaseActivity
 
 class SongsActivity : BaseActivity() {
 
+    lateinit var viewModel: SongsViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        appComponent.inject(this)
+        initializeViewModel()
+    }
+
+    private fun initializeViewModel() {
+        viewModel = ViewModelProviders.of(this, viewModelFactory)[SongsViewModel::class.java]
+        viewModel.songs.observe(this, Observer(::showSongs))
+        viewModel.failure.observe(this, Observer(::handleFailure))
+    }
+
+    private fun showSongs(songs: List<Song>?) {
+        Log.d("SongsRes", songs.toString())
+    }
+
+    private fun handleFailure(failure: Failure?) {
+        when (failure) {
+            is Failure.NetworkConnection -> showFailure(getString(R.string.network_connection))
+            is Failure.ServerError -> showFailure(getString(R.string.server_error))
+        }
+    }
+
+    private fun showFailure(message: String) {
+        //TODO: show errors in a snack bar
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
+
+    private fun search() {
+        val term = "Michael Jackson"
+        viewModel.search(term)
     }
 }
